f5aa95680a34b44f7519c9eb63a4c64fe0314ece
Kelly Christian <ms.kchristian86@gmail.com>
2022-10-17 21:57:52 -0300

    updated js file with x and o

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -10,4 +10,26 @@ document.addEventListener("DOMContentLoaded",function(){
             sq[k].className= "square";
         }
     }
+    let cx ="X";//CHEMICAL X POWER PUFF REFERENCE
+    let ns =document.getElementsByClassName("square");
+
+    for(let l=0; l<ns.length; l++){
+        ns[l].addEventListener("click",function(){
+            if(ns[l].innerHTML==""){
+                if(cx=="X"){
+                    ns[l].innerHTML="X";
+                    ns[l].classList.add("X");
+                    cx="O";
+                } else {
+                    ns[l].innerHTML="O";
+                    ns[l].classList.add("O");
+                    cx="X";
+                }
+            }
+        })
+    }
+
+ 
+    
 })
+
